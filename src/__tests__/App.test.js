92824a3ed411cd4c8f3613d2e5c1f1508ee05833
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-15 13:32:20 -0500

    use accertions from jest

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,7 +6,11 @@ import { App } from '../components/App';
 import { shallow } from 'enzyme';
 
 describe('<App />', () => {
-	it('should render App', () => {
-		const wrapper = shallow(<App />);
-	});
+  const wrapper = shallow(<App />);
+
+	it('should contain 1 h3 element', () => {
+    expect(wrapper.find('h3').length).toBe(1)
+  });
+  
+  console.log(wrapper.debug())
 });
