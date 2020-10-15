2c67155e2b2183d3a088a87a68f3327a19325213
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-15 12:43:25 -0500

    finish set up
    *fis bugs
    *install combatible dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-//import html webpack plugin
+//import webpack plugins
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-//css rules
-cssRules = {
-	test: /\.css$/i,
-	use: ['style-loader', 'css-loader'],
+const cssRules = {
+  test: /\.css$/i,
+  use: ['style-loader', 'css-loader'],
 };
 
-//babel rules
-babelRules = {
-	test: /\.js$/,
-	exclude: /node_modules/,
-	loader: 'babel-loader',
+const babelRules = {
+  test: /\.(js)$/,
+  exclude: /node_modules/,
+  loader: 'babel-loader',
 };
 
-//ise webpack config and export it
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.[contentHash].js',
-	},
-	plugins: [
-		new HtmlWebPackPlugin({
-			title: 'enzyme practice',
-			template: './public/index.html',
-		}),
-	],
-	devServer: {
-		port: 3000,
-	},
+  entry: './src/index.js',
+  output: {
+    filename: 'app.[contentHash].js',
+  },
+  module: {
+    rules: [babelRules, cssRules],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'marketing site',
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    port: 3000,
+  },
 };
