de18abb3dc025fa1fcb3b3554811ac1bf144b52a
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-15 14:08:10 -0500

    leanr about enzymes selectors

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,23 +6,27 @@ import { App } from '../components/App';
 import { shallow } from 'enzyme';
 
 describe('<App />', () => {
-  const wrapper = shallow(<App />);
+	const wrapper = shallow(<App />);
 
 	it('should contain 1 h3 element', () => {
-    expect(wrapper.find('h3').length).toBe(1)
+		expect(wrapper.find('h3').length).toBe(1);
+	});
+
+	it('should contain title class', () => {
+		expect(wrapper.find('.title').exists()).toBe(true);
+	});
+
+	it('should the correct text', () => {
+		expect(wrapper.find('.title').text()).toBe("I'm the app component");
+	});
+
+	it('should have only children', () => {
+		expect(wrapper.find('.title').children().length).toBe(1);
+	});
+
+	it('should bee sub title component', () => {
+		expect(wrapper.find('SubTitle[text="hey"]').exists()).toBe(true);
   });
 
-  it('should contain title class', () => {
-    expect(wrapper.find('.title').exists()).toBe(true)
-  });
-
-  it('should the correct text', () => {
-    expect(wrapper.find('.title').text()).toBe("I'm the app component")
-  })
-
-  it('should have only children', () => {
-    expect(wrapper.find('.title').children().length).toBe(1)
-  })
-  
-  console.log(wrapper.debug())
+	console.log(wrapper.debug());
 });
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,15 @@
 //import react
 import React from 'react';
 
+const SubTitle = ({text}) => <h4>{text}</h4>;
+
 //export and create app component
-export const App = () => <h3 className='title'>I'm the app component</h3>;
+export const App = () => {
+	return (
+		<>
+			<h3 className='title'>I'm the app component</h3>
+			<h2>Hey there</h2>
+			<SubTitle text='hey' />
+		</>
+	);
+};
