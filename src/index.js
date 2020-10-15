95b81426d263d35c097e99985dea2d1fdda782fc
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-15 12:35:11 -0500

    install enzyme and jest

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//import react
+import React from 'react';
+//import render method
+import { render } from 'react-dom';
+
+//crete root const
+const root = document.getElementById('root');
+
+//render the app
+render(<h1>I'm alive</h1>, root);
