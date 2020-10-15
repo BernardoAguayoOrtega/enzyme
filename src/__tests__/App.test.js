2734c78fecdb5b2f6278ba69c59c9c1c74d2950f
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-15 13:44:24 -0500

    add other tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -15,6 +15,14 @@ describe('<App />', () => {
   it('should contain title class', () => {
     expect(wrapper.find('.title').exists()).toBe(true)
   });
+
+  it('should the correct text', () => {
+    expect(wrapper.find('.title').text()).toBe("I'm the app component")
+  })
+
+  it('should have only children', () => {
+    expect(wrapper.find('.title').children().length).toBe(1)
+  })
   
   console.log(wrapper.debug())
 });
